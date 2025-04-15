b614d0b943999820debe802375d26925d2bf8422
Zyad <zyadhammad531@gmail.com>
2025-04-15 17:20:24 +0200

    remove test for koin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,10 @@
 package org.example
 
 import org.example.di.dataModule
-import org.example.logic.RecipesRepository
 import org.koin.core.context.startKoin
-import org.koin.java.KoinJavaComponent.getKoin
 
 fun main() {
     startKoin {
         modules(dataModule)
     }
-
-    val recipesRepository: RecipesRepository = getKoin().get()
-
-    println(recipesRepository.getAllRecipes().first())
 }
