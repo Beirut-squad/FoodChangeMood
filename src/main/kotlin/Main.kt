201560e58dbd254ba0fcd9b3d15753fc7e18f69d
Bassant Mohammed <bassant.22230@gmail.com>
2025-04-16 13:06:00 +0200

    solve conflict

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,10 @@
 package org.example
 
-import org.example.data.CsvParser
-import org.example.data.CsvReader
-import org.example.data.FileNames.FOOD_CSV_FILE
-import org.example.data.FileNames.PROCESSED_CSV_FILE
-import org.example.data.RecipesRepositoryCsvImpl
 import org.example.di.dataModule
 import org.example.di.useCaseModule
-import org.example.logic.EasyFoodSuggestionUseCase
 import org.example.ui.FoodChangeMoodUi
 import org.koin.core.context.startKoin
 import org.koin.mp.KoinPlatform.getKoin
-import java.io.File
 
 fun main() {
     startKoin {
