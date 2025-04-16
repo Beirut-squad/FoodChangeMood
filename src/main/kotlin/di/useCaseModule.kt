394c5dda25e36e1e624c43fa5dd2a3f78f620c80
Zyad <zyadhammad531@gmail.com>
2025-04-16 14:11:23 +0200

    remove redundunt ui module

--- src/main/kotlin/di/useCaseModule.kt
+++ src/main/kotlin/di/useCaseModule.kt
@@ -1,8 +1,16 @@
 package org.example.di
 
 import org.example.logic.EasyFoodSuggestionUseCase
+import org.example.logic.Validator
+import org.example.logic.use_case.GymHelperUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
     single { EasyFoodSuggestionUseCase(get()) }
+    single {
+        Validator()
+    }
+    single {
+        GymHelperUseCase(get())
+    }
 }
