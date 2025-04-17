3a6fb38a50db8eb7b32a338b1e056f921446d716
Hazem Alkateb <hazem22-1@hotmail.com>
2025-04-17 10:03:08 +0300

    complete work on the search recipe by date use case in ui -add some functions to the program interface

--- src/main/kotlin/ui/FoodChangeMoodUi.kt
+++ src/main/kotlin/ui/FoodChangeMoodUi.kt
@@ -6,6 +6,7 @@ import org.example.error.RecipeNotFoundException
 import org.example.logic.EasyFoodSuggestionUseCase
 import org.example.logic.RandomTenRecipesIncludePotatoUseCase
 import org.example.logic.SearchRecipeByDateUseCase
+import org.example.model.Recipe
 import java.text.ParseException
 import java.time.format.DateTimeParseException
 
@@ -98,12 +99,8 @@ class FoodChangeMoodUi(
         println("Do you want to get details of a specific recipe? (Y/N)")
         readlnOrNull()?.lowercase().let {answer->
             when(answer){
-                "y" -> {
-                    searchRecipeByID()
-                }
-                "n" -> {
-                    isRunning = true
-                }
+                "y" -> { searchRecipeByID() }
+                "n" -> { isRunning = true }
                 else -> {
                     println("Invalid choice")
                     askUserIfHeWantDetailsOfRecipe()
@@ -116,29 +113,53 @@ class FoodChangeMoodUi(
         try {
             println("Enter the ID of the recipe whose details you want to see:")
             readlnOrNull()?.let {enteredID->
-                val recipe = searchRecipeByDateUseCase.viewDetailsOfRecipeByID(enteredID)
-                println("Recipe Details: ------------------------------------------------\nName: ${recipe.name}\n" +
-                        "Minutes: ${recipe.minutes}\nContributor Id: ${recipe.contributorId}\n" +
-                        "Submitted Date: ${recipe.submittedDate}\nTags:\n${recipe.tags}\n" +
-                        "Nutrition:\nCalories = ${recipe.nutrition?.calories}\t" +
-                        "Total Fat = ${recipe.nutrition?.totalFat}\t" +
-                        "Sugar = ${recipe.nutrition?.sugar}\t" +
-                        "Sodium = ${recipe.nutrition?.sodium}\t" +
-                        "Protein = ${recipe.nutrition?.protein}\t" +
-                        "Saturated Fat = ${recipe.nutrition?.saturatedFat}\t" +
-                        "Carbohydrates = ${recipe.nutrition?.carbohydrates}\n" +
-                        "Number Of Steps: ${recipe.numberOfSteps}\n" +
-                        "Steps:\n${recipe.steps}\n" +
-                        "Description: ${recipe.description}\n" +
-                        "Ingredients:\n${recipe.ingredients}\n" +
-                        "Number Of Ingredients: ${recipe.numberOfIngredients}")
-                isRunning = true
-            } ?: println("Enter valid id !")
+                val idAsNumber = enteredID.toIntOrNull() ?: 0
+                if (idAsNumber == 0){
+                    println("Enter valid id !")
+                    askToBackToMainMenu()
+                }else{
+                    val recipe = searchRecipeByDateUseCase.viewDetailsOfRecipeByID(enteredID)
+                    printRecipe(recipe)
+                    isRunning = true
+                }
+            } ?: { println("Enter valid id !")
+                askToBackToMainMenu()
+            }
         }catch (e: RecipeNotFoundException){
+            // ask to exit from this menu to main menu
             println(e.message)
-            searchRecipeByID()
+            askToBackToMainMenu()
         }
     }
 
+    private fun askToBackToMainMenu(){
+        println("Are you need to back to main menu ? (Y/N)")
+        readlnOrNull()?.lowercase().let {answer->
+            when(answer){
+                "y" -> { isRunning = true }
+                "n" -> { searchRecipeByID() }
+                else -> {
+                    println("Invalid choice")
+                }
+            }
+        }
+    }
 
+    private fun printRecipe(recipe: Recipe){
+        println("Recipe Details: ------------------------------------------------\nName: ${recipe.name}\n" +
+                "Minutes: ${recipe.minutes}\nContributor Id: ${recipe.contributorId}\n" +
+                "Submitted Date: ${recipe.submittedDate}\nTags:\n${recipe.tags}\n" +
+                "Nutrition:\nCalories = ${recipe.nutrition?.calories}\t" +
+                "Total Fat = ${recipe.nutrition?.totalFat}\t" +
+                "Sugar = ${recipe.nutrition?.sugar}\t" +
+                "Sodium = ${recipe.nutrition?.sodium}\t" +
+                "Protein = ${recipe.nutrition?.protein}\t" +
+                "Saturated Fat = ${recipe.nutrition?.saturatedFat}\t" +
+                "Carbohydrates = ${recipe.nutrition?.carbohydrates}\n" +
+                "Number Of Steps: ${recipe.numberOfSteps}\n" +
+                "Steps:\n${recipe.steps}\n" +
+                "Description: ${recipe.description}\n" +
+                "Ingredients:\n${recipe.ingredients}\n" +
+                "Number Of Ingredients: ${recipe.numberOfIngredients}")
+    }
 }
