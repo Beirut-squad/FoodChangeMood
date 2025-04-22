8a5394ddb40eaded2638bf5454c6324bf014479a
Bassant Mohammed <bassant.22230@gmail.com>
2025-04-22 13:17:53 +0200

    add a test check if function return good approximated values

--- src/test/kotlin/logic/use_case/GymHelperUseCaseTest.kt
+++ src/test/kotlin/logic/use_case/GymHelperUseCaseTest.kt
@@ -1,10 +1,13 @@
 package logic.use_case
 
 
+import com.google.common.truth.Truth.assertThat
+import io.mockk.every
 import io.mockk.mockk
 import org.example.logic.RecipesRepository
 import org.example.logic.use_case.GymHelperUseCase
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 class GymHelperUseCaseTest {
     private lateinit var repository: RecipesRepository
@@ -15,5 +18,27 @@ class GymHelperUseCaseTest {
         repository = mockk()
         gymHelperUseCase = GymHelperUseCase(repository)
     }
+
+    @Test
+    fun `should return recipe that contains approximate values when calories and protein are both float `() {
+        //Give
+        every { repository.getAllRecipes() } returns listOf(
+            createGymHelper(5f, 9f),
+            createGymHelper(100f, 10f),
+            createGymHelper(null, 200f)
+        )
+        val calories = 9f
+        val protein = 10f
+        //When
+        val result =
+            gymHelperUseCase
+                .getRecipesMatchOrApproximateAmountOfCaloriesAndProtein(calories, protein)
+        //Then
+        assertThat(result)
+            .containsExactly(
+                createGymHelper(5f, 9f))
+    }
+
+
 
 }
