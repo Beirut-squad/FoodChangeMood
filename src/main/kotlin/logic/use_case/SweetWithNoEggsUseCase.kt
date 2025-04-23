2b013ab517f09c4e54ce96c62d1e795574309486
Hazem Alkateb <hazem22-1@hotmail.com>
2025-04-23 17:09:06 +0300

    make unit test for SweetWithNoEggsUseCase.kt - edit on findSweetsFreeEggs function to make it throw Exception when not found sweets - add function to create recipe for sweet with no egg on CreateRecipeHelper.kt

--- src/main/kotlin/logic/use_case/SweetWithNoEggsUseCase.kt
+++ src/main/kotlin/logic/use_case/SweetWithNoEggsUseCase.kt
@@ -1,5 +1,6 @@
 package org.example.logic.use_case
 
+import org.example.error.RecipeNotFoundException
 import org.example.logic.RecipesRepository
 import org.example.model.Recipe
 import org.example.model.isComplete
@@ -13,9 +14,9 @@ class SweetWithNoEggsUseCase (
             .filter { it.isComplete() }
             .filter { recipe ->
                 !recipe.name!!.contains("egg", ignoreCase = true) &&
-                        !recipe.description?.contains("egg", ignoreCase = true)!! &&
+                        !recipe.description!!.contains("egg", ignoreCase = true) &&
                         (recipe.name.contains("sweet", ignoreCase = true) ||
                                 recipe.description.contains("sweet", ignoreCase = true))
-            }.shuffled().firstOrNull()
+            }.shuffled().firstOrNull() ?: throw RecipeNotFoundException("there is no Sweets with egg-less")
     }
 }
