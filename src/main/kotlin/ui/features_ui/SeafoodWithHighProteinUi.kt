081b8d097588cd5724a3e88f32124970efa85aa9
Bassant Mohammed <bassant.22230@gmail.com>
2025-04-23 15:49:34 +0200

    refactor seafood use case and seafood ui classes

--- src/main/kotlin/ui/features_ui/SeafoodWithHighProteinUi.kt
+++ src/main/kotlin/ui/features_ui/SeafoodWithHighProteinUi.kt
@@ -1,22 +1,28 @@
 package org.example.ui.features_ui
 
+import org.example.error.RecipeNotFoundException
 import org.example.logic.use_case.SeafoodWithHighProteinUseCase
 import org.example.utils.Colors
 
-class SeafoodWithHighProteinUi (
+class SeafoodWithHighProteinUi(
     private val seafoodWithHighProteinUseCase: SeafoodWithHighProteinUseCase,
     private val colors: Colors
-
-    ){
-     fun show() {
-        println(colors.blue("Loading..."))
-        seafoodWithHighProteinUseCase.getSeafoodWithProteinRecipes()
-            .forEachIndexed { index, recipe ->
+) {
+    fun show() {
+        try {
+            println(colors.blue("Loading..."))
+            val recipes = seafoodWithHighProteinUseCase.getSeafoodWithProteinRecipes()
+            recipes.forEachIndexed { index, recipe ->
                 println(
-                    colors.green("${index + 1}. " +
-                            "Recipe Name: \n\t${recipe.name} " +
-                            "\n\tProtein Amount: \n\t${recipe.nutrition?.protein}"
-                ))
+                    colors.green(
+                        "${index + 1}. " +
+                                "Recipe Name: \n\t${recipe.name} " +
+                                "\n\tProtein Amount: \n\t${recipe.nutrition?.protein}"
+                    )
+                )
             }
+        } catch (e: RecipeNotFoundException) {
+            println(colors.red("Error: ${e.message}"))
+        }
     }
 }
