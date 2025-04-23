4a433a999304db18f39e5bd1ffcda2a16fb4cbec
mostafa khaled <mostafakhaled2172003@gmail.com>
2025-04-23 17:51:24 +0200

    add viewer and reader

---
+++ src/main/kotlin/ui/Viewer.kt
@@ -0,0 +1,11 @@
+package org.example.ui
+
+class Viewer {
+    fun printOutput(output: String) {
+        print(output)
+    }
+
+    fun printOutputWithNewLine(output: String) {
+        println(output)
+    }
+}
