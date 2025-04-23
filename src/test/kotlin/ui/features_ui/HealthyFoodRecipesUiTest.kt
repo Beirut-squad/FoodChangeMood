0d700b8b987b6119ab062c5dcc5d1e08231267d9
yousef_osama11 <oyousef709@gmail.com>
2025-04-23 20:49:25 +0200

    add more test cases

--- src/test/kotlin/ui/features_ui/HealthyFoodRecipesUiTest.kt
+++ src/test/kotlin/ui/features_ui/HealthyFoodRecipesUiTest.kt
@@ -1,16 +1,17 @@
 package ui.features_ui
 
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.spyk
-import io.mockk.verify
+import io.mockk.*
 import org.example.logic.Validator
 import org.example.logic.use_case.HealthyRecipesUseCase
+import org.example.model.Recipe
+import org.example.ui.RecipeFormatter
 import org.example.ui.features_ui.HealthyFoodRecipesUi
 import org.example.utils.Colors
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import java.util.logging.Formatter
 
 class HealthyFoodRecipesUiTest {
 
@@ -18,7 +19,6 @@ class HealthyFoodRecipesUiTest {
     private lateinit var healthyRecipesUseCase: HealthyRecipesUseCase
     private lateinit var colors: Colors
     private lateinit var healthyFoodRecipesUi: HealthyFoodRecipesUi
-
     @BeforeEach
     fun setup() {
         validator = mockk(relaxed = true)
@@ -76,13 +76,55 @@ class HealthyFoodRecipesUiTest {
 
     @Test
     fun `should show loading message when displayRecipeInfo is called`(){
-
-
         // When
         healthyFoodRecipesUi.displayRecipeInfo(5)
 
         // Then
         verify { healthyFoodRecipesUi.outputPrinter(colors.blue("Loading...")) }
+    }
+
+
+    @Test
+    fun `should show a no recipes available message when there is no healthy recipes found`(){
+        // Given
+        every { healthyRecipesUseCase.getHealthyRecipes(any()) } returns emptyList()
+
+        // When
+        healthyFoodRecipesUi.displayRecipeInfo(5)
+
+        // Then
+        verify { healthyFoodRecipesUi.outputPrinter(colors.red("No Recipes Available")) }
+    }
+
+//    @Disabled
+//    @Test
+//    fun `should show the recipes info when there is available recipes`(){
+//        // Given
+//        val fakeRecipesList = List(5) { mockk<Recipe>() }
+//        every { healthyRecipesUseCase.getHealthyRecipes(any())} returns fakeRecipesList
+//        every { recipeFormatter.format(any()) } returns "Formated recipe"
+//
+//        // When
+//        healthyFoodRecipesUi.displayRecipeInfo(5)
+//
+//        // Then
+//        verify(exactly = 5) { healthyFoodRecipesUi.outputPrinter(any()) }
+//    }
+
+    @Test
+    fun `should show the recipes info when there is available recipes`() {
+        // Given
+        val fakeRecipesList = List(5) { mockk<Recipe>(relaxed = true) }
+
+        every { healthyRecipesUseCase.getHealthyRecipes(any()) } returns fakeRecipesList
+        mockkObject(RecipeFormatter)
+        every { RecipeFormatter.format(any()) } returns "Fake Recipe Info"
+
+        // When
+        healthyFoodRecipesUi.displayRecipeInfo(5)
+
+        // Then
+        verify(exactly = 5) { healthyFoodRecipesUi.outputPrinter("Fake Recipe Info") }
     }
 
 
