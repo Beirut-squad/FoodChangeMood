22b1be5f54161fbdea347b21ad6e025997fdd67f
Bassant Mohammed <bassant.22230@gmail.com>
2025-04-24 06:57:22 +0200

    verify UI shows loading message in blue color

--- src/test/kotlin/ui/features_ui/SeafoodWithHighProteinUiTest.kt
+++ src/test/kotlin/ui/features_ui/SeafoodWithHighProteinUiTest.kt
@@ -1,10 +1,13 @@
 package ui.features_ui
 
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verifyOrder
 import org.example.logic.use_case.SeafoodWithHighProteinUseCase
 import org.example.ui.Viewer
 import org.example.ui.features_ui.SeafoodWithHighProteinUi
 import org.junit.jupiter.api.BeforeEach
+import kotlin.test.Test
 
 class SeafoodWithHighProteinUiTest {
 
@@ -16,5 +19,28 @@ class SeafoodWithHighProteinUiTest {
     fun setup() {
         seafoodWithHighProteinUi = SeafoodWithHighProteinUi(dummyUseCase, viewer)
     }
-    
+
+    @Test
+    fun `should return recipes with protein information and name in correct format`() {
+        // Given
+        val recipes = listOf(
+            createSeafoodHelper("Grilled Salmon", 30f),
+            createSeafoodHelper("Shrimp Scampi", 25f)
+        )
+        every { dummyUseCase.getSeafoodWithProteinRecipes() } returns recipes
+
+        // When
+        seafoodWithHighProteinUi.show()
+
+        // Then
+        verifyOrder {
+            viewer.printCorrectOutput(
+                "1. Recipe Name: Grilled Salmon \n\tProtein Amount: \n\t30.0"
+            )
+            viewer.printCorrectOutput(
+                "2. Recipe Name: Shrimp Scampi \n\tProtein Amount: \n\t25.0"
+            )
+        }
+    }
+
 }
