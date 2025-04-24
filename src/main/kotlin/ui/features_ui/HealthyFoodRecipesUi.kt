3c7c4966d80c489f37650b62f9a6fee29974d93c
yousef_osama11 <oyousef709@gmail.com>
2025-04-24 15:39:50 +0200

    refactor HealthyFoodRecipesUiTest -refactor HealthyFoodRecipesUi -add readInt to Reader

--- src/main/kotlin/ui/features_ui/HealthyFoodRecipesUi.kt
+++ src/main/kotlin/ui/features_ui/HealthyFoodRecipesUi.kt
@@ -2,6 +2,7 @@ package org.example.ui.features_ui
 
 import org.example.logic.Validator
 import org.example.logic.use_case.HealthyRecipesUseCase
+import org.example.ui.Reader
 import org.example.ui.RecipeFormatter
 import org.example.ui.Viewer
 import org.example.utils.Colors
@@ -9,41 +10,27 @@ import org.example.utils.Colors
 class HealthyFoodRecipesUi(
     private val validator: Validator,
     private val healthyRecipesUseCase: HealthyRecipesUseCase,
-    private val colors: Colors,
-    private val viewer: Viewer
+    private val viewer: Viewer,
+    private val reader: Reader,
 
 ) {
     fun show() {
-//        outputPrinter(colors.cyan("Enter the number of meals you want"))
         viewer.printTitle("Enter the number of meals you want")
-        val count = getUserInput()
+        val count = reader.readInt()
 
         if (count == null || !validator.validateRecipesCountInput(count)) {
-//            outputPrinter(colors.red("Invalid Input, Enter a Positive Number"))
             viewer.printError("Invalid Input, Enter a Positive Number")
         } else
-            displayRecipeInfo(count)
+            displayRecipeInfo(count.toInt())
     }
 
-    fun outputPrinter(message: String){
-        println(message)
-    }
-
-    fun displayRecipeInfo(count: Int) {
-//        outputPrinter(colors.blue("Loading..."))
+    private fun displayRecipeInfo(count: Int) {
         viewer.printLoader("Loading...")
         val healthyRecipes = healthyRecipesUseCase.getHealthyRecipes(count)
         if (healthyRecipes.isEmpty()) {
-//            outputPrinter(colors.red("No Recipes Available"))
             viewer.printError("No Recipes Available")
         } else healthyRecipes.forEach {
-//            outputPrinter(RecipeFormatter.format(it))
             viewer.printPlainText(RecipeFormatter.format(it))
         }
     }
-
-     fun getUserInput(): Int? {
-        return readlnOrNull()?.toIntOrNull()
-    }
-
 }
--- src/test/kotlin/ui/features_ui/HealthyFoodRecipesUiTest.kt
+++ src/test/kotlin/ui/features_ui/HealthyFoodRecipesUiTest.kt
@@ -4,6 +4,7 @@ import io.mockk.*
 import org.example.logic.Validator
 import org.example.logic.use_case.HealthyRecipesUseCase
 import org.example.model.Recipe
+import org.example.ui.Reader
 import org.example.ui.RecipeFormatter
 import org.example.ui.Viewer
 import org.example.ui.features_ui.HealthyFoodRecipesUi
@@ -18,34 +19,35 @@ class HealthyFoodRecipesUiTest {
 
     private lateinit var validator: Validator
     private lateinit var healthyRecipesUseCase: HealthyRecipesUseCase
-    private lateinit var colors: Colors
     private lateinit var healthyFoodRecipesUi: HealthyFoodRecipesUi
     private lateinit var viewer: Viewer
+    private lateinit var reader: Reader
 
     @BeforeEach
     fun setup() {
         validator = mockk(relaxed = true)
         healthyRecipesUseCase = mockk(relaxed = true)
-        colors = mockk(relaxed = true)
         viewer = mockk(relaxed = true)
-        healthyFoodRecipesUi = spyk(HealthyFoodRecipesUi(validator, healthyRecipesUseCase, colors , viewer))
+        reader = mockk(relaxed = true)
+
+        healthyFoodRecipesUi = HealthyFoodRecipesUi(validator, healthyRecipesUseCase, viewer, reader)
 
     }
 
     @Test
-    fun `should show a cyan-colored question for the number of meals the user want to see`(){
+    fun `should show a cyan-colored question for the number of meals the user want to see`() {
+
         // When
         healthyFoodRecipesUi.show()
 
         // Then
-        verify { viewer.printTitle("Enter the number of meals you want")}
+        verify { viewer.printTitle("Enter the number of meals you want") }
     }
 
-
     @Test
-    fun `should show a red invalid input message when the user input is null`(){
+    fun `should show a red invalid input message when the user input is null`() {
         // Given
-        every { healthyFoodRecipesUi.getUserInput() } returns null
+        every { reader.readInt() } returns null
 
         // When
         healthyFoodRecipesUi.show()
@@ -55,8 +57,9 @@ class HealthyFoodRecipesUiTest {
     }
 
     @Test
-    fun `should show a red invalid input message when the user input is invalid number`(){
+    fun `should show a red invalid input message when the user input is invalid number`() {
         // Given
+        every { reader.readInt() } returns 5
         every { validator.validateRecipesCountInput(any()) } returns false
 
         // When
@@ -67,35 +70,28 @@ class HealthyFoodRecipesUiTest {
     }
 
     @Test
-    fun `should call displayRecipeInfo when the user enters a valid input`(){
+    fun `should show loading message when the user enters a valid input`() {
         // Given
-        every { healthyFoodRecipesUi.getUserInput() } returns 5
+        every { reader.readInt() } returns 5
         every { validator.validateRecipesCountInput(any()) } returns true
 
         // When
         healthyFoodRecipesUi.show()
 
         // Then
-        verify { healthyFoodRecipesUi.displayRecipeInfo(any()) }
-    }
 
-    @Test
-    fun `should show loading message when displayRecipeInfo is called`(){
-        // When
-        healthyFoodRecipesUi.displayRecipeInfo(5)
-
-        // Then
         verify { viewer.printLoader("Loading...") }
     }
 
-
     @Test
-    fun `should show a no recipes available message when there is no healthy recipes found`(){
+    fun `should show a no recipes available message when there is no healthy recipes found`() {
         // Given
+        every { reader.readInt() } returns 5
+        every { validator.validateRecipesCountInput(any()) } returns true
         every { healthyRecipesUseCase.getHealthyRecipes(any()) } returns emptyList()
 
         // When
-        healthyFoodRecipesUi.displayRecipeInfo(5)
+        healthyFoodRecipesUi.show()
 
         // Then
         verify { viewer.printError("No Recipes Available") }
@@ -104,14 +100,16 @@ class HealthyFoodRecipesUiTest {
     @Test
     fun `should show the recipes info when there is available recipes`() {
         // Given
+        every { reader.readInt() } returns 5
+        every { validator.validateRecipesCountInput(any()) } returns true
         val fakeRecipesList = List(5) { mockk<Recipe>(relaxed = true) }
-
         every { healthyRecipesUseCase.getHealthyRecipes(any()) } returns fakeRecipesList
+
         mockkObject(RecipeFormatter)
         every { RecipeFormatter.format(any()) } returns "Fake Recipe Info"
 
         // When
-        healthyFoodRecipesUi.displayRecipeInfo(5)
+        healthyFoodRecipesUi.show()
 
         // Then
         verify(exactly = 5) { viewer.printPlainText("Fake Recipe Info") }
