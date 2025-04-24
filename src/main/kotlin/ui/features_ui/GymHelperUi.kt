f88a76114426c14dce260ef9b34f7fcbb8fb265b
mostafa khaled <mostafakhaled2172003@gmail.com>
2025-04-24 16:44:36 +0200

    implement Display interface in gym helper ui

--- src/main/kotlin/ui/features_ui/GymHelperUi.kt
+++ src/main/kotlin/ui/features_ui/GymHelperUi.kt
@@ -5,26 +5,27 @@ import org.example.logic.use_case.GymHelperUseCase
 import org.example.model.Recipe
 import org.example.ui.Reader
 import org.example.ui.Viewer
+import ui.Display
 
-class GymHelperUi (
+class GymHelperUi(
     private val gymHelperUseCase: GymHelperUseCase,
     private val validator: Validator,
     private val viewer: Viewer,
     private val reader: Reader
 
-    ){
-    fun show() {
+) : Display {
+    override fun show() {
 
         viewer.printTitle("Gym helper: Get meals that match the protein and calories amounts you choose or close to them.")
         while (true) {
-            viewer.printLoader("Enter the amount of protein: ",false)
+            viewer.printLoader("Enter the amount of protein: ", false)
             val protein = reader.readInput()
 
-            viewer.printLoader("Enter the amount of calories: ",false)
+            viewer.printLoader("Enter the amount of calories: ", false)
             val calories = reader.readInput()
 
             if (validator.validateGymHelperInput(calories, protein)) {
-                val recipes =  gymHelperUseCase.getRecipesMatchOrApproximateAmountOfCaloriesAndProtein(
+                val recipes = gymHelperUseCase.getRecipesMatchOrApproximateAmountOfCaloriesAndProtein(
                     calories = calories?.toFloat() ?: 0f,
                     protein = protein?.toFloat() ?: 0f
                 )
@@ -56,16 +57,16 @@ class GymHelperUi (
     }
 
     private fun displayIngredients(ingredients: List<String>) {
-        viewer.printCorrectOutput("Ingredients: ",false)
+        viewer.printCorrectOutput("Ingredients: ", false)
         ingredients.forEach {
-            viewer.printCorrectOutput("$it, ",false)
+            viewer.printCorrectOutput("$it, ", false)
         }
     }
 
     private fun displaySteps(steps: List<String>) {
         viewer.printCorrectOutput("How to Make: ")
         steps.forEachIndexed { stepIndex, step ->
-            viewer.printCorrectOutput("Step ${stepIndex + 1}: ",false)
+            viewer.printCorrectOutput("Step ${stepIndex + 1}: ", false)
             viewer.printPlainText(step)
         }
     }
