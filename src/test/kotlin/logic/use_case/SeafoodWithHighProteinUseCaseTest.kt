94770665b10d5bb6642ebfaffe674bdce30d8702
yousef_osama11 <oyousef709@gmail.com>
2025-04-24 20:59:01 +0200

    add setup function

--- src/test/kotlin/logic/use_case/SeafoodWithHighProteinUseCaseTest.kt
+++ src/test/kotlin/logic/use_case/SeafoodWithHighProteinUseCaseTest.kt
@@ -1,5 +1,41 @@
 package logic.use_case
 
+import io.mockk.every
+import io.mockk.mockk
+import org.example.logic.RecipesRepository
+import org.example.logic.use_case.SeafoodWithHighProteinUseCase
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
 
-class SeafoodWithHighProteinUseCaseTest
+class SeafoodWithHighProteinUseCaseTest{
+    private lateinit var recipesRepository: RecipesRepository
+    private lateinit var seafoodWithHighProteinUseCase: SeafoodWithHighProteinUseCase
+
+
+    @BeforeEach
+    fun setup(){
+        recipesRepository = mockk(relaxed = true)
+        seafoodWithHighProteinUseCase = SeafoodWithHighProteinUseCase(recipesRepository)
+    }
+
+    @Disabled
+    @Test
+    fun `should return list of seafood protein recipes if found`(){
+        // Given
+        every { recipesRepository.getAllRecipes() } returns
+
+
+        // When
+        seafoodWithHighProteinUseCase.getSeafoodWithProteinRecipes()
+
+        // Then
+
+
+    }
+
+
+
+
+}
