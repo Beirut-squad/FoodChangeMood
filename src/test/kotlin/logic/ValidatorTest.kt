39c62fc02afcddaf1bd3718e06d217835e1ddff5
Bassant Mohammed <bassant.22230@gmail.com>
2025-04-24 13:25:43 +0200

    validate gym helper Input returns true for valid numeric calories and protein

---
+++ src/test/kotlin/logic/ValidatorTest.kt
@@ -0,0 +1,24 @@
+package logic
+
+import org.example.logic.Validator
+import org.junit.jupiter.api.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class ValidatorTest {
+    private var validator = Validator()
+
+    @Test
+    fun `should return true when gym helper use case called and add protein and calories`() {
+        //Given
+        val calories = "30.0"
+        val protein = "22.0"
+        //When
+        val result = validator.validateGymHelperInput(calories, protein)
+        //Then
+        assertTrue { result }
+
+    }
+
+
+}
