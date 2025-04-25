ca6982f59f9a90450519ed7e47bec82908b468fb
sohahamdy2 <sohahamdy1310@gmail.com>
2025-04-25 19:21:56 +0300

    Add a case blocked

--- src/main/kotlin/logic/use_case/SearchByNameUseCase.kt
+++ src/main/kotlin/logic/use_case/SearchByNameUseCase.kt
@@ -12,6 +12,10 @@ class SearchByNameUseCase(
 ) {
     fun searchRecipeByName(foodNameToSearch: String): List<Recipe>? {
         val normalizedQuery = foodNameToSearch.trim().lowercase()
+        if (normalizedQuery.isBlank()) {
+            return emptyList()
+        }
+
         //  Get all words from Trie
         val allTrieWords = trie.getAllWords()
         //  Use KMP to filter matching words
