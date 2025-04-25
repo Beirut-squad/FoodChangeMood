05c7f757e636442632f1f969e9806d6c7b07b5f2
yousef_osama11 <oyousef709@gmail.com>
2025-04-25 19:07:10 +0300

    refactor makeRecipeHelper

--- src/test/kotlin/logic/use_case/makeRecipeHelper.kt
+++ src/test/kotlin/logic/use_case/makeRecipeHelper.kt
@@ -61,12 +61,3 @@ fun createNutrition(
     )
 }
 
-//data class Nutrition(
-//    val calories: Float?,
-//    val totalFat: Float?,
-//    val sugar: Float?,
-//    val sodium: Float?,
-//    val protein: Float?,
-//    val saturatedFat: Float?,
-//    val carbohydrates: Float?,
-//)
